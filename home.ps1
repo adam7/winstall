966c618032f64baee26a66a61764f1e57db5ff16
Adam Cooper <mail@admcpr.com>
2020-11-30 20:35:21 +0100

    Create home.ps1

---
+++ home.ps1
@@ -0,0 +1,14 @@
+winget install WhatsApp.WhatsApp  ; 
+winget install Microsoft.VisualStudioCode  ; 
+winget install QL-Win.QuickLook  ; 
+winget install Spotify.Spotify  ; 
+winget install VideoLAN.VLC  ; 
+winget install Mozilla.Firefox  ; 
+winget install Microsoft.WindowsTerminal  ; 
+winget install Plex.Plex  ; 
+winget install Valve.Steam  ; 
+winget install Microsoft.PowerToys  ; 
+winget install DelugeTeam.Deluge  ; 
+winget install Docker.DockerDesktop  ; 
+winget install Git.Git  ; 
+winget install TickTick.TickTick 
