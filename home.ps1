d7edb50997d8a6c6cb946dd53b8c94e98230a06a
Adam Cooper <mail@admcpr.com>
2022-03-07 10:40:54 +0100

    Re-organise, add brave, node, volta; remove firefox(😢) , spotify

--- home.ps1
+++ home.ps1
@@ -1,17 +1,21 @@
-winget install WhatsApp.WhatsApp  ; 
-winget install Microsoft.VisualStudioCode  ; 
-winget install QL-Win.QuickLook  ; 
-winget install Spotify.Spotify  ; 
-winget install VideoLAN.VLC  ; 
-winget install Mozilla.Firefox  ; 
-winget install Plex.Plex  ; 
-winget install Valve.Steam  ; 
-winget install Microsoft.PowerToys  ; 
-winget install DelugeTeam.Deluge  ; 
-winget install Docker.DockerDesktop  ; 
-winget install Git.Git  ; 
-winget install TickTick.TickTick ;
-winget install Ubuntu ;
-winget install Microsoft.PowerShell ;
-winget install JanDeDobbeleer.OhMyPosh ;
-winget install GoLang.Go ;
+winget install QL-Win.QuickLook -h --accept-package-agreements ; 
+winget install Microsoft.PowerToys -h --accept-package-agreements  ; 
+winget install BraveSoftware.BraveBrowser -h --accept-package-agreements;
+winget install VideoLAN.VLC -h --accept-package-agreements  ; 
+winget install Plex.Plex -h --accept-package-agreements  ; 
+winget install WhatsApp.WhatsApp  -h --accept-package-agreements  ; 
+winget install Valve.Steam -h --accept-package-agreements  ; 
+winget install TickTick.TickTick -h --accept-package-agreements ;
+
+# Dev tools
+winget install Microsoft.VisualStudioCode  -h --accept-package-agreements  ; 
+winget install Docker.DockerDesktop -h --accept-package-agreements  ; 
+winget install Git.Git -h --accept-package-agreements  ; 
+winget install Ubuntu -h --accept-package-agreements;
+winget install Microsoft.PowerShell -h --accept-package-agreements ;
+winget install JanDeDobbeleer.OhMyPosh -h --accept-package-agreements ;
+
+# Dev languages
+winget install GoLang.Go -h --accept-package-agreements ;
+winget install OpenJS.NodeJS.LTS -h --accept-package-agreements;
+winget install Volta.Volta -h --accept-package-agreements;
