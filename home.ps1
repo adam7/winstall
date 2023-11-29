421167b88341363c1f7dce40f47d822310b91908
Adam Cooper <mail@admcpr.com>
2023-11-29 13:20:20 +0100

    Update home.ps1

--- home.ps1
+++ home.ps1
@@ -1,4 +1,3 @@
-winget install QL-Win.QuickLook -h --accept-package-agreements ; 
 winget install Microsoft.PowerToys -h --accept-package-agreements  ; 
 winget install BraveSoftware.BraveBrowser -h --accept-package-agreements;
 winget install VideoLAN.VLC -h --accept-package-agreements  ; 
@@ -6,13 +5,14 @@ winget install Plex.Plex -h --accept-package-agreements  ;
 winget install WhatsApp.WhatsApp  -h --accept-package-agreements  ; 
 winget install Valve.Steam -h --accept-package-agreements  ; 
 winget install TickTick.TickTick -h --accept-package-agreements ;
-winget install SumatraPDF -h --accept-package-agreements ;
+winget install NativeInstruments.NativeAccess -h --accept-package-agreements ;
 
 # Dev tools
 winget install Microsoft.VisualStudioCode  -h --accept-package-agreements  ; 
 winget install Docker.DockerDesktop -h --accept-package-agreements  ; 
 winget install Git.Git -h --accept-package-agreements  ; 
-winget install Ubuntu -h --accept-package-agreements;
+winget install GitHub.cli -h --accept-package-agreements  ; 
+winget install Canonical.Ubuntu.2204 -h --accept-package-agreements;
 winget install Microsoft.PowerShell -h --accept-package-agreements ;
 winget install JanDeDobbeleer.OhMyPosh -h --accept-package-agreements ;
 
