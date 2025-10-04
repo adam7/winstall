34da41962107f34dbd7e83e08bf2b4ab0c343d15
Adam Cooper <mail@admcpr.com>
2025-10-04 17:43:09 +0200

    Update home

--- home.ps1
+++ home.ps1
@@ -3,20 +3,22 @@ winget install Brave.Brave -h --accept-package-agreements;
 winget install VideoLAN.VLC -h --accept-package-agreements  ; 
 winget install Plex.Plex -h --accept-package-agreements  ; 
 winget install Valve.Steam -h --accept-package-agreements  ; 
-winget install TickTick.TickTick -h --accept-package-agreements ;
 winget install NativeInstruments.NativeAccess -h --accept-package-agreements ;
 winget install Elgato.StreamDeck -h --accept-package-agreements ;
 winget install xanderfrangos.twinkletray -h --accept-package-agreements ;
+winget install Tailscale.Tailscale -h --accept-package-agreements ;
+winget install Logitech.LogiTune -h --accept-package-agreements ;
+winget install Mozilla.Thunderbird -h --accept-package-agreements ;
+winget install REALiX.HWiNFO -h --accept-package-agreements ;
+Bitwarden.Bitwarden
 
 # Dev tools
 winget install Microsoft.VisualStudioCode  -h --accept-package-agreements  ; 
 winget install Docker.DockerDesktop -h --accept-package-agreements  ; 
 winget install Git.Git -h --accept-package-agreements  ; 
 winget install GitHub.cli -h --accept-package-agreements  ; 
-winget install Canonical.Ubuntu.2204 -h --accept-package-agreements;
+winget install Canonical.Ubuntu -h --accept-package-agreements;
 winget install Microsoft.PowerShell -h --accept-package-agreements ;
-winget install JanDeDobbeleer.OhMyPosh -h --accept-package-agreements ;
-
-# Dev languages
+winget install starship -h --accept-package-agreements ;
 winget install GoLang.Go -h --accept-package-agreements ;
 winget install Volta.Volta -h --accept-package-agreements;
